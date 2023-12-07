9afba315f3aa4292bd4412e8be10a057451ff10f
Ramanuj Jindal <121759925+ramanuj07@users.noreply.github.com>
2023-12-07 19:44:16 +0530

    Update countVowels.js

--- medium/countVowels.js
+++ medium/countVowels.js
@@ -10,7 +10,7 @@ function countVowels(str) {
 
   let newStr = str.replace(/\s/g, '').toLowerCase();
   // in javascript regular expressions are used denoted by this symbol --> // 
-  // anything inside regular expressions would be searched. In this case \s means that it would replace all the whitespace or tab characters with an empty string.  Here /g means that it is applicable globally which means it won't stop after finding the first whitespace character and would rather iterate throughout the string
+  // anything inside regular expressions would be searched. In this case \s means that it would replace all the whitespace or tab characters with an empty string.  Here `g` means that it is applicable globally which means it won't stop after finding the first whitespace character and would rather iterate throughout the string
   // we also mentioned to convert it to lowercase so that we can get uniformity in all types of string whether they are uppercase or lowercase or a mix of both. this is done using toLowerCase() in javascript
 
   for(let i=0; i<newStr.length; i++) {
